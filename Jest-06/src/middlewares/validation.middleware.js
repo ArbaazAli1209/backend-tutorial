7bfb137a09ec0f707ad7741b9a51757ee1f08c68
Arbaaz <arbaaz8158@gmail.com>
2026-07-30 22:10:24 +0530

    Validation.

---
+++ Jest-06/src/middlewares/validation.middleware.js
@@ -0,0 +1,31 @@
+const { body, validationResult } = require('express-validator');
+const { isLength } = require('validator');
+
+async function validateResult(req, res, next) {
+
+    const errors = validationResult(req);
+
+    if(!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    next();
+
+}
+
+const registerUserValidationRules = [
+    body("username")
+        .isString()
+        .withMessage("Username must be a String")
+        .isLength({ min: 3, max: 20 })
+        .withMessage("Username must be between 3 and 20 characters"),
+    body("email")
+        .isEmail()
+        .withMessage("Invalid email address"),
+    body("password")
+        .isLength({ min: 6 })
+        .withMessage("Password must me atleast 6 characters long"),
+    validateResult
+]
+
+module.exports = { registerUserValidationRules };
